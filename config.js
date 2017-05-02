e0b9f252b0957c2891cc52ff4f008fa1f1a7a7e6
Dawid <dejwidkdk@gmail.com>
2017-05-02 13:50:08 +0200

    This is where it all begins...

---
+++ config.js
@@ -0,0 +1,6 @@
+module.exports = {
+
+	'secret': 'inzynierski-project',
+	'database': 'mongodb://Server:Server123456789@ds151060.mlab.com:51060/user-activity-analyzer'
+
+};
--- index.js
+++ index.js
@@ -1,20 +1,112 @@
-var express = require('express');
-var app = express();
+var express 	= require('express');
+var app         = express();
+var bodyParser  = require('body-parser');
+var morgan      = require('morgan');
+var mongoose    = require('mongoose');
 
-app.set('port', (process.env.PORT || 5000));
+var jwt    = require('jsonwebtoken'); // used to create, sign, and verify tokens
+var config = require('./config'); // get our config file
+var User   = require('./app/models/user'); // get our mongoose model
 
-app.use(express.static(__dirname + '/public'));
+var port = process.env.PORT || 8080;
+mongoose.connect(config.database); // connect to database
+app.set('superSecret', config.secret); // secret variable
 
-// views is directory for all template files
-app.set('views', __dirname + '/views');
-app.set('view engine', 'ejs');
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
 
-app.get('/', function(request, response) {
-  response.render('pages/index');
+app.use(morgan('dev'));
+
+app.post('/setup', function(req, res) {
+	var login = req.body.login;
+	var password = req.body.password;
+
+	var nick = new User({
+		login: login,
+		password: password,
+		admin: false
+	});
+
+	nick.save(function(err) {
+		if (err) throw err;
+
+		console.log('User saved successfully');
+		res.json({ success: true });
+	});
 });
 
-app.listen(app.get('port'), function() {
-  console.log('Node app is running on port', app.get('port'));
+app.get('/', function(req, res) {
+	res.send('Hello! The API is at http://localhost:' + port + '/api');
 });
 
+// get an instance of the router for api routes
+var apiRoutes = express.Router();
 
+apiRoutes.post('/authenticate', function(req, res) {
+
+	console.log("Authentication for login: " + req.body.login);
+	console.log("Authentication for password: " + req.body.password);
+
+	User.findOne({
+		login: req.body.login
+	}, function(err, user) {
+
+		if (err) throw err;
+
+		if (!user) {
+			res.json({ success: false, message: 'Authentication failed. User not found.' });
+		} else if (user) {
+
+			if (user.password != req.body.password) {
+				res.json({ success: false, message: 'Authentication failed. Wrong password.' });
+			} else {
+
+				var token = jwt.sign(user, app.get('superSecret'), {
+					expiresIn: 3600 // 1 hour
+				});
+
+				res.json({
+					success: true,
+					message: 'Enjoy your token!',
+					token: token
+				});
+			}
+		}
+	});
+});
+
+apiRoutes.use(function(req, res, next) {
+	var token = req.body.token || req.param('token');
+
+	if (token) {
+
+		jwt.verify(token, app.get('superSecret'), function(err, decoded) {
+			if (err) {
+				return res.json({ success: false, message: 'Failed to authenticate token.' });
+			} else {
+				req.decoded = decoded;
+				next();
+			}
+		});
+
+	} else {
+
+		return res.status(403).send({
+			success: false,
+			message: 'No token provided.'
+		});
+	}
+});
+
+apiRoutes.get('/', function(req, res) {
+	res.json({ message: 'Welcome to the API!' });
+});
+
+apiRoutes.get('/check', function(req, res) {
+	res.json(req.decoded);
+});
+
+app.use('/api', apiRoutes);
+
+app.listen(port);
+console.log('Server running at http://localhost:' + port);
