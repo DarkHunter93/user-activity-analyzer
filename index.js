2ab7ea661dfe51fbf18e799d124c7d9b9e732ffd
Dawid <dejwidkdk@gmail.com>
2017-05-10 14:09:47 +0200

    Some improvements

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ var app         = express();
 var bodyParser  = require('body-parser');
 var morgan      = require('morgan');
 var mongoose    = require('mongoose');
-var jwt    			= require('jsonwebtoken');
+var JWT    			= require('jsonwebtoken');
 var winston 		= require('winston');
 var path 				= require('path');
 var config 			= require('./config');
@@ -20,64 +20,70 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, 'public')));
 app.use(morgan('dev'));
 
-app.post('/setup', function(req, res) {
-	winston.info(`POST /setup with body: ${JSON.stringify(req.body)}`);
-	console.log(`POST /setup with body: ${JSON.stringify(req.body)}`);
-
-	if (req.body.login == undefined || req.body.login == null) {
-		return res.json({ success: false, message: 'req.body.login is null or undefined' });
-	} else if (req.body.password == undefined || req.body.password == null) {
-		return res.json({ success: false, message: 'req.body.password is null or undefined' });
-	} else {
-		var login = req.body.login;
-		var password = req.body.password;
-
-		var nick = new User({
-			login: login,
-			password: password,
-			admin: false
-		});
-
-		nick.save(function(err) {
-			if (err) throw err;
-
-			console.log('User saved successfully');
-			res.json({ success: true });
-		});
-	}
-});
-
 app.get('/', function(req, res) {
   winston.info('GET /');
   res.send('Hello! The API is at http://localhost:' + port + '/api');
 });
 
+app.post('/register', function(req, res) {
+	winston.info(`POST /setup with body: ${JSON.stringify(req.body)}`);
+
+	if (req.body.login == undefined || req.body.login == null) {
+		return res.status(400).json({ success: false, message: 'Login is null or undefined' });
+	} else if (req.body.password == undefined || req.body.password == null) {
+		return res.status(400).json({ success: false, message: 'Password is null or undefined' });
+	} else {
+		var login = req.body.login;
+		var password = req.body.password;
+
+		User.findOne({
+			login: req.body.login
+		}, (error, user) => {
+			if (error) {
+				throw error;
+			} else if (user) {
+				return res.status(400).json({ success: false, message: 'Login is already in use' });
+			} else {
+
+				var user = new User({
+					login: login,
+					password: password,
+					admin: false
+				});
+
+				user.save((error) => {
+					if (error) throw error;
+
+					res.status(201).json({ success: true, message: 'User registered successfully' });
+				});
+			}
+		});
+	}
+});
+
 var apiRoutes = express.Router();
 
-apiRoutes.post('/authenticate', function(req, res) {
+apiRoutes.post('/login', function(req, res) {
 	winston.info(`POST /api/authenticate with body: ${JSON.stringify(req.body)}`);
 
 	User.findOne({
 		login: req.body.login
-	}, function(err, user) {
+	}, (error, user) => {
 
-		if (err) throw err;
-
-		if (!user) {
-			console.log(`Authentication for user with login: ${req.body.login} and password: ${req.body.password} failed. User not found`);
-			res.json({ success: false, message: 'Authentication failed. User not found.' });
+		if (error) {
+			throw error;
+		} else if (!user) {
+			res.status(400).json({ success: false, message: 'Authentication failed. User not found.' });
 		} else if (user) {
 
 			if (user.password != req.body.password) {
-				console.log(`Authentication for user with login: ${req.body.login} and password: ${req.body.password} failed. Wrong password`);
-				res.json({ success: false, message: 'Authentication failed. Wrong password.' });
+				res.status(400).json({ success: false, message: 'Authentication failed. Wrong password.' });
 			} else {
 
-				var token = jwt.sign(user, app.get('superSecret'), {
-					expiresIn: 3600 // 1 hour
+				var token = JWT.sign(user, app.get('superSecret'), {
+					expiresIn: 3600
 				});
 				winston.info(`Token for ${req.body.login}: ${token}`);
-				console.log(`Token for ${req.body.login}: ${token}`);
 
 				res.status(200).json({
 					success: true,
@@ -90,21 +96,18 @@ apiRoutes.post('/authenticate', function(req, res) {
 });
 
 apiRoutes.use(function(req, res, next) {
-	var token = req.body.token || req.param('token');
+	var token = req.body.token || req.params.token;
 
 	if (token) {
-
-		jwt.verify(token, app.get('superSecret'), function(err, decoded) {
-			if (err) {
-				return res.json({ success: false, message: 'Failed to authenticate token.' });
+		JWT.verify(token, app.get('superSecret'), (error, decoded) => {
+			if (error) {
+				return res.json({ success: false, message: 'Failed to authenticate token.'});
 			} else {
 				req.decoded = decoded;
 				next();
 			}
 		});
-
 	} else {
-
 		return res.status(403).send({
 			success: false,
 			message: 'No token provided.'
@@ -115,6 +118,14 @@ apiRoutes.use(function(req, res, next) {
 apiRoutes.get('/check', function(req, res) {
 	winston.info(`GET /api/check with query.token: ${req.query.token}`);
 	res.json(req.decoded);
+});
+
+apiRoutes.post('/check', function(req, res) {
+	res.json({
+		login: req.decoded._doc.login,
+		password: req.decoded._doc.password,
+		admin: req.decoded._doc.admin
+	});
 });
 
 app.use('/api', apiRoutes);
